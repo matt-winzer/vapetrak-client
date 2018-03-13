61dde779e246d0abe514a027eebc1fae976511d2
Matt Winzer <matt.winzer@gMWad5ddf.local>
2018-03-12 23:39:37 -0600

    created simple header & Calculator component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src='https://img0.etsystatic.com/215/0/13221305/il_570xN.1209981028_jgff.jpg' className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to VapeTrak</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Calculator />
       </div>
     );
   }
---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="Calculator">
+                <p>Hello World</p>   
+            </div>
+        );
+    }
+}
+
+export default Calculator;
